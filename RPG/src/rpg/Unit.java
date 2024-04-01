01c0af5668474fbdb551cef5ddeabb89d4aa55f1
ksy7615 <sykim7615@daum.net>
2024-04-01 17:03:56 +0900

    Create Variable Of Class Unit

--- RPG/src/rpg/Unit.java
+++ RPG/src/rpg/Unit.java
@@ -1,5 +1,10 @@
 package rpg;
 
-public class Unit {
-
+public abstract class Unit {
+	private String name;
+//	public final int MAX_HP;
+	private int hp;
+	private int power;
+	private int defence;
+	
 }
