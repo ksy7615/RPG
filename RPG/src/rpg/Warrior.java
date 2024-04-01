fe55b4a9d089bb4c23af8d7d5d7eb5e2157838d9
ksy7615 <sykim7615@daum.net>
2024-04-01 18:02:09 +0900

    Add variable potion Of Class Warrior

--- RPG/src/rpg/Warrior.java
+++ RPG/src/rpg/Warrior.java
@@ -1,14 +1,15 @@
 package rpg;
 
 public class Warrior extends Unit {
+	private int potion;
 
-	public Warrior(String name, int hp, int power, int defence) {
+	public Warrior(String name, int hp, int power, int defence, int potion) {
 		super(name, hp, power, defence);
+		this.potion = potion;
 	}
 
 	@Override
 	public void defaultAttack() {
-		// TODO Auto-generated method stub
 		
 	}
 
