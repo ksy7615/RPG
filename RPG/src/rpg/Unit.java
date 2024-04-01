152e4176edb90af5d4dee36339b98a018f799579
ksy7615 <sykim7615@daum.net>
2024-04-01 17:09:24 +0900

    Create Constructor Of Class Unit

--- RPG/src/rpg/Unit.java
+++ RPG/src/rpg/Unit.java
@@ -2,9 +2,17 @@ package rpg;
 
 public abstract class Unit {
 	private String name;
-//	public final int MAX_HP;
+	public final int MAX_HP;
 	private int hp;
 	private int power;
 	private int defence;
+	
+	public Unit(String name, int hp, int power, int defence) {
+		this.name = name;
+		MAX_HP = hp;
+		this.hp = hp;
+		this.power = power;
+		this.defence = defence;
+	}
 	
 }
