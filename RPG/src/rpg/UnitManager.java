42535db5b714b2723d55bcb94929bcc50b95c8eb
ksy7615 <sykim7615@daum.net>
2024-04-02 17:09:36 +0900

    Add Constructor Of Class UnitManager (before balance patch)

--- RPG/src/rpg/UnitManager.java
+++ RPG/src/rpg/UnitManager.java
@@ -5,5 +5,12 @@ import java.util.ArrayList;
 public class UnitManager {
 	private ArrayList<PlayerUnit> playerList = new ArrayList<>();
 	private ArrayList<MonsterUnit> monsterList = new ArrayList<>();
+	
+	public UnitManager() {
+		Warrior warrior = new Warrior("전사", 1, 1, 1, 1, 1, 1, 1);
+		Tanker tanker = new Tanker("탱커", 1, 1, 1, 1, 1, 1, 1);
+		Wizard wizard = new Wizard("마법사", 1, 1, 1, 1, 1, 1, 1);
+		Healer healer = new Healer("힐러", 1, 1, 1, 1, 1, 1, 1);
+	}
 
 }
