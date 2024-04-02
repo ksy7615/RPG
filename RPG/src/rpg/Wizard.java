bfcdcb9fd12c2441dcb6b9551e85d1aa7fe0cef8
ksy7615 <sykim7615@daum.net>
2024-04-02 11:32:23 +0900

    Modify extends Class Of Class Wizard

--- RPG/src/rpg/Wizard.java
+++ RPG/src/rpg/Wizard.java
@@ -1,9 +1,27 @@
 package rpg;
 
-public class Wizard extends Unit {
+public class Wizard extends PlayerUnit {
 
-	public Wizard(String name, int hp, int power, int defence) {
-		super(name, hp, power, defence);
+	public Wizard(String name, int hp, int mp, int power, int defence, int level, int exp) {
+		super(name, hp, mp, power, defence, level, exp);
+	}
+
+	@Override
+	public void defaultAttack(Unit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(Unit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recovery() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
