c465ca21c8807c91c27c0f8c37cba158816de60b
ksy7615 <sykim7615@daum.net>
2024-04-02 11:47:50 +0900

    Add defaultAttack Of Class Wizard

--- RPG/src/rpg/Wizard.java
+++ RPG/src/rpg/Wizard.java
@@ -9,13 +9,25 @@ public class Wizard extends PlayerUnit {
 	}
 
 	@Override
-	public void defaultAttack(Unit unit) {
-		// TODO Auto-generated method stub
-		
+	public void defaultAttack(Unit monster) {
+		this.hit = random.nextInt(this.power);
+
+		if (this.hit == 0)
+			System.err.println("MISS");
+
+		// 몬스터의 방어력 비례 피해량 수정 필요
+		monster.setHp(monster.getHp() - this.hit);
+
+		if (monster.getHp() <= 0) {
+			monster.setHp(0);
+		}
+
+		System.out.println(String.format("[%s]의 기본 공격\n", super.getName()));
+		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", monster.getName(), this.hit));
 	}
 
 	@Override
-	public void skill(Unit unit) {
+	public void skill(Unit monster) {
 		// TODO Auto-generated method stub
 		
 	}
