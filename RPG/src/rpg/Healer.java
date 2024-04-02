2a1017c5e6b14d4b05db4880895983e034fbc489
ksy7615 <sykim7615@daum.net>
2024-04-02 10:08:00 +0900

    Modify constructor Of Class Healer

--- RPG/src/rpg/Healer.java
+++ RPG/src/rpg/Healer.java
@@ -2,8 +2,22 @@ package rpg;
 
 public class Healer extends Unit {
 
-	public Healer(String name, int hp, int power, int defence) {
-		super(name, hp, power, defence);
+	public Healer(String name, int hp, int power, int defence, int level, int exp, int potion, int mp) {
+		super(name, hp, power, defence, level, exp);
 	}
+
+	@Override
+	public void defaultAttack(Unit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(Unit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 }
