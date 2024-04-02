1790a75dd524a21880ca8f2c8644e3861ca96cd4
ksy7615 <sykim7615@daum.net>
2024-04-02 12:47:27 +0900

    Add Constructor Of Class MonsterUnit

--- RPG/src/rpg/MosterUnit.java
+++ RPG/src/rpg/MosterUnit.java
@@ -1,5 +1,9 @@
 package rpg;
 
-abstract class MosterUnit {
+abstract class MosterUnit extends Unit {
+
+	public MosterUnit(String name, int hp, int power, int defence, int level) {
+		super(name, hp, power, defence, level);
+	}
 	
 }
