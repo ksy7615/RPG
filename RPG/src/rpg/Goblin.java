321a8c1fca8cc8b1442997db6e506059083ee783
ksy7615 <sykim7615@daum.net>
2024-04-02 15:03:22 +0900

    Modify extends Class Of Class Goblin

--- RPG/src/rpg/Goblin.java
+++ RPG/src/rpg/Goblin.java
@@ -1,9 +1,15 @@
 package rpg;
 
-public class Goblin extends Unit {
+public class Goblin extends MonsterUnit {
 
-	public Goblin(String name, int hp, int power, int defence) {
-		super(name, hp, power, defence);
+	public Goblin(String name, int hp, int power, int defence, int level) {
+		super(name, hp, power, defence, level);
+	}
+
+	@Override
+	public void attackPlayer(Unit unit) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
