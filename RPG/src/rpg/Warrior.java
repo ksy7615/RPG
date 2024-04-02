ed8ccb7f5b7ca3f7de1506e880b2ac8b5baa79fc
ksy7615 <sykim7615@daum.net>
2024-04-02 12:34:16 +0900

    Modify defaultAttack Of Class Warrior

--- RPG/src/rpg/Warrior.java
+++ RPG/src/rpg/Warrior.java
@@ -10,10 +10,13 @@ public class Warrior extends PlayerUnit {
 
 	@Override
 	public void defaultAttack(Unit monster) {
+		System.out.println(String.format("[%s]의 기본 공격\n", super.getName()));
 		this.hit = random.nextInt(this.power);
 
-		if (this.hit == 0)
+		if (this.hit == 0) {
 			System.err.println("MISS");
+			return;
+		}
 
 		// 몬스터의 방어력 비례 피해량 수정 필요
 		monster.setHp(monster.getHp() - this.hit);
@@ -21,12 +24,10 @@ public class Warrior extends PlayerUnit {
 		if (monster.getHp() <= 0) {
 			monster.setHp(0);
 		}
-
-		System.out.println(String.format("[%s]의 기본 공격\n", super.getName()));
 		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", monster.getName(), this.hit));
 	}
 
-	// 용사의 스킬 > 공격력 3배 & 미스X 유효타 & 몬스터 방어력 무시
+	// 용사의 스킬 > 기본 공격의 3배 데미지 & 미스X 유효타 & 몬스터 방어력 무시
 	@Override
 	public void skill(Unit monster) {
 		System.out.println(String.format("[%s]의 스킬 [Slash Blast]\n", super.getName()));
@@ -37,7 +38,6 @@ public class Warrior extends PlayerUnit {
 		if (monster.getHp() <= 0) {
 			monster.setHp(0);
 		}
-
 		this.setMp(this.getMp() - 20);
 		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", monster.getName(), this.hit));
 		System.out.println(String.format("[%s]의 현재 HP %s\n", monster.getName(), monster));
