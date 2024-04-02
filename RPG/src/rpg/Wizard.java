e06c91d9e6349f4b78880dc31829b86ae40b0aef
ksy7615 <sykim7615@daum.net>
2024-04-02 12:08:23 +0900

    Add skill Of Class Wizard

--- RPG/src/rpg/Wizard.java
+++ RPG/src/rpg/Wizard.java
@@ -26,16 +26,28 @@ public class Wizard extends PlayerUnit {
 		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", monster.getName(), this.hit));
 	}
 
+	// 모든 몹들 공격하게 하기 >> 게임에서 for문으로 구현
 	@Override
 	public void skill(Unit monster) {
-		// TODO Auto-generated method stub
+		System.out.println(String.format("[%s]의 스킬 [Energy Bolt]\n", super.getName()));
+		// 마법사 마법력 > 공격력 스탯의 3배로 잡아서 무조건 유효타 인정
+		this.energy = random.nextInt(this.power * 3) + 1;
 		
+		// (밸런스를 위해..) 몬스터의 방어력 비례 피해량 수정 필요
+		monster.setHp(monster.getHp() - this.energy);
+		
+		if(monster.getHp() <= 0) {
+			monster.setHp(0);
+		}
+		
+		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", monster.getName(), this.energy));
+		System.out.println(String.format("[%s]의 현재 HP %s\n", monster.getName(), monster));
 	}
 
 	@Override
 	public void recovery() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
