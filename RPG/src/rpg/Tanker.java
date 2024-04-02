6491c0fac7f96fc800e9e12b85d8ca35ce4b3ffb
ksy7615 <sykim7615@daum.net>
2024-04-02 12:10:58 +0900

    Modify extends Class Of Class Tanker

--- RPG/src/rpg/Tanker.java
+++ RPG/src/rpg/Tanker.java
@@ -1,9 +1,28 @@
 package rpg;
 
-public class Tanker extends Unit {
+public class Tanker extends PlayerUnit {
 
-	public Tanker(String name, int hp, int power, int defence) {
-		super(name, hp, power, defence);
+	public Tanker(String name, int hp, int mp, int power, int defence, int level, int exp, int potion) {
+		super(name, hp, mp, power, defence, level, exp, potion);
 	}
+
+	@Override
+	public void defaultAttack(Unit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(Unit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recovery() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
