46d4fba0e530d4b5e16bf538d58b877b316ffbc8
ksy7615 <sykim7615@daum.net>
2024-04-02 15:12:27 +0900

    Modify extends Class & Add variable shield Of Class Cerberus

--- RPG/src/rpg/Cerberus.java
+++ RPG/src/rpg/Cerberus.java
@@ -1,9 +1,23 @@
 package rpg;
 
-public class Cerberus extends Unit {
+public class Cerberus extends MonsterUnit implements BossMob {
+	private int shield;
 
-	public Cerberus(String name, int hp, int power, int defence) {
-		super(name, hp, power, defence);
+	public Cerberus(String name, int hp, int power, int defence, int level, int shield) {
+		super(name, hp, power, defence, level);
+		this.shield = shield;
 	}
 
+	@Override
+	public void attackPlayer(Unit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void skill(Unit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
