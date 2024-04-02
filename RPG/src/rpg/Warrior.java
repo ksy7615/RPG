5dab3bb8945dd86e72fb4549d0d6d8546e0f969a
ksy7615 <sykim7615@daum.net>
2024-04-02 10:39:39 +0900

    Modify parameter location & override Of Class Warrior

--- RPG/src/rpg/Warrior.java
+++ RPG/src/rpg/Warrior.java
@@ -3,34 +3,45 @@ package rpg;
 public class Warrior extends PlayerUnit {
 	private int hit;
 	private int slice;
-	
+
 	private int potion;
 	private final int MAX_MP;
 	private int mp;
 
-	public Warrior(String name, int hp, int power, int defence, int level, int exp, int potion, int mp) {
+	public Warrior(String name, int hp, int mp, int power, int defence, int level, int exp, int potion) {
 		super(name, hp, power, defence, level, exp);
-		this.potion = potion;
 		MAX_MP = mp;
 		this.mp = mp;
+		this.potion = potion;
 	}
 
 	@Override
-	public void defaultAttack(Unit unit) {
-		// TODO Auto-generated method stub
-		
+	public void defaultAttack(Unit monster) {
+		this.hit = random.nextInt(this.power);
+
+		if (this.hit == 0)
+			System.err.println("MISS");
+
+		monster.setHp(monster.getHp() - this.hit);
+
+		if (monster.getHp() <= 0) {
+			monster.setHp(0);
+		}
+
+		System.out.println(String.format("[%s]의 기본 공격", super.getName()));
+		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.", monster.getName()));
 	}
 
 	@Override
-	public void skill(Unit unit) {
+	public void skill(Unit monster) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void recovery() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
