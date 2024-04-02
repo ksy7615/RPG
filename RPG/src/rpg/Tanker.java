eee0f0bc79c68fbb420ea09d1c0a94ed22e7f528
ksy7615 <sykim7615@daum.net>
2024-04-02 12:13:14 +0900

    Add defaultAttack Of Class Tanker

--- RPG/src/rpg/Tanker.java
+++ RPG/src/rpg/Tanker.java
@@ -1,15 +1,28 @@
 package rpg;
 
 public class Tanker extends PlayerUnit {
+	private int hit;
+	private int shield;
 
 	public Tanker(String name, int hp, int mp, int power, int defence, int level, int exp, int potion) {
 		super(name, hp, mp, power, defence, level, exp, potion);
 	}
 
 	@Override
-	public void defaultAttack(Unit unit) {
-		// TODO Auto-generated method stub
-		
+	public void defaultAttack(Unit monster) {
+		this.hit = random.nextInt(this.power);
+
+		if (this.hit == 0)
+			System.err.println("MISS");
+
+		// 몬스터의 방어력 비례 피해량 수정 필요
+		monster.setHp(monster.getHp() - this.hit);
+
+		if (monster.getHp() <= 0) {
+			monster.setHp(0);
+		}
+		System.out.println(String.format("[%s]의 기본 공격\n", super.getName()));
+		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", monster.getName(), this.hit));
 	}
 
 	@Override
