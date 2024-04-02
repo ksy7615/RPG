017d04dab257de51769f42614658e7df775830f2
ksy7615 <sykim7615@daum.net>
2024-04-02 10:05:32 +0900

    Add defaultAttack Of Class Warrior

--- RPG/src/rpg/Warrior.java
+++ RPG/src/rpg/Warrior.java
@@ -16,11 +16,23 @@ public class Warrior extends Unit {
 	}
 
 	@Override
-	public void defaultAttack() {
+	public void defaultAttack(Unit monster) {
+		this.hit = random.nextInt(this.power);
+		
+		if(this.hit == 0)
+			System.err.println("MISS");
+		
+		monster.setHp(monster.getHp() - this.hit);
+		
+		if(monster.getHp() <= 0)
+			monster.setHp(0);
+		
+		System.out.println(String.format("[%s]의 기본 공격", super.getName()));
+		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.", monster.getName()));
 	}
 
 	@Override
-	public void skill() {
+	public void skill(Unit monster) {
 		
 	}
 
