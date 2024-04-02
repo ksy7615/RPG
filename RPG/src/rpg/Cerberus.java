6f57b92744d8681223985056c85646d848607d5b
ksy7615 <sykim7615@daum.net>
2024-04-02 15:14:26 +0900

    Add attackPlayer Of Class Cerberus

--- RPG/src/rpg/Cerberus.java
+++ RPG/src/rpg/Cerberus.java
@@ -9,13 +9,28 @@ public class Cerberus extends MonsterUnit implements BossMob {
 	}
 
 	@Override
-	public void attackPlayer(Unit unit) {
-		// TODO Auto-generated method stub
+	public void attackPlayer(Unit player) {
+		System.out.println(String.format("[%s]의 공격\n", this.getName()));
+		int hit = random.nextInt(power) + 1;
 		
+		hit = hit - player.getDefence()/5;
+		
+		if(hit <= 0) {
+			hit = 0;
+			System.out.println("MISS");
+		}
+		
+		player.setHp(player.getHp() - hit);
+
+		if (player.getHp() <= 0) {
+			player.setHp(0);
+		}
+		
+		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", player.getName(), hit));
 	}
 	
 	@Override
-	public void skill(Unit unit) {
+	public void skill(Unit player) {
 		// TODO Auto-generated method stub
 		
 	}
