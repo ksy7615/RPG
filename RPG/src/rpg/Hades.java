05f815d5bbaf0661a57997eab931d4602f71e543
ksy7615 <sykim7615@daum.net>
2024-04-02 16:00:33 +0900

    Modify extends Class Of Class Hades

--- RPG/src/rpg/Hades.java
+++ RPG/src/rpg/Hades.java
@@ -1,9 +1,21 @@
 package rpg;
 
-public class Hades extends Unit {
+public class Hades extends MonsterUnit implements BossMob {
 
-	public Hades(String name, int hp, int power, int defence) {
-		super(name, hp, power, defence);
+	public Hades(String name, int hp, int power, int defence, int level) {
+		super(name, hp, power, defence, level);
+	}
+
+	@Override
+	public void attackPlayer(Unit unit) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void skill(Unit unit) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
