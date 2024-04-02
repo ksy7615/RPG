9310fa6a2a42bd410344d073c7dfe0ba25a237d9
ksy7615 <sykim7615@daum.net>
2024-04-02 14:04:46 +0900

    Modify extends Class Of Mimic

--- RPG/src/rpg/Mimic.java
+++ RPG/src/rpg/Mimic.java
@@ -1,9 +1,21 @@
 package rpg;
 
-public class Mimic extends Unit {
+public class Mimic extends MonsterUnit {
 
-	public Mimic(String name, int hp, int power, int defence) {
-		super(name, hp, power, defence);
+	public Mimic(String name, int hp, int power, int defence, int level) {
+		super(name, hp, power, defence, level);
+	}
+
+	@Override
+	public void defaultAttack(Unit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(Unit unit) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
