94ca7ac3fb0baa942a8569a74bf0145b71f8229d
ksy7615 <sykim7615@daum.net>
2024-04-02 11:15:56 +0900

    Add recovery method Of Class Warrior

--- RPG/src/rpg/Warrior.java
+++ RPG/src/rpg/Warrior.java
@@ -24,14 +24,14 @@ public class Warrior extends PlayerUnit {
 			monster.setHp(0);
 		}
 
-		System.out.println(String.format("[%s]의 기본 공격", super.getName()));
-		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.", monster.getName(), this.hit));
+		System.out.println(String.format("[%s]의 기본 공격\n", super.getName()));
+		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", monster.getName(), this.hit));
 	}
 
 	// 용사의 스킬 > 공격력 3배 & 미스X 유효타 & 몬스터 방어력 무시
 	@Override
 	public void skill(Unit monster) {
-		System.out.println(String.format("[%s]의 스킬 [Slash Blast]", super.getName()));
+		System.out.println(String.format("[%s]의 스킬 [Slash Blast]\n", super.getName()));
 		this.slash = 3 * (random.nextInt(this.power) + 1);
 		
 		monster.setHp(monster.getHp() - this.slash);
@@ -41,14 +41,22 @@ public class Warrior extends PlayerUnit {
 		}
 		
 		this.setMp(this.getMp() - 20);
-		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.", monster.getName(), this.hit));
-		System.out.println(String.format("[%s]의 현재 HP %s", monster.getName(), monster));
+		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", monster.getName(), this.hit));
+		System.out.println(String.format("[%s]의 현재 HP %s\n", monster.getName(), monster));
 	}
 
+	// 포션 HP, MP 모두 최대로 회복
 	@Override
 	public void recovery() {
-		// TODO Auto-generated method stub
-
+		if (this.potion > 0) {
+			setHp(MAX_HP);
+			setMp(MAX_MP);
+			this.potion--;
+			System.out.println(String.format("[%s] %s\n", super.getName(), this));
+			System.out.println("완전히 회복되었습니다!\n");
+		} else if (this.potion == 0) {
+			System.err.println("텅 -\n");
+		}
 	}
 
 }
