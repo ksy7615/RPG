64c198259829243cccae04f1a9118542276df2b0
ksy7615 <sykim7615@daum.net>
2024-04-02 10:50:33 +0900

    Modify Constructor(by Add variable Of Class Unit) Of Class PlayerUnit

--- RPG/src/rpg/PlayerUnit.java
+++ RPG/src/rpg/PlayerUnit.java
@@ -2,8 +2,8 @@ package rpg;
 
 abstract class PlayerUnit extends Unit {
 
-	public PlayerUnit(String name, int hp, int power, int defence, int level, int exp) {
-		super(name, hp, power, defence, level, exp);
+	public PlayerUnit(String name, int hp, int mp, int power, int defence, int level, int exp) {
+		super(name, hp, mp, power, defence, level, exp);
 	}
 
 	public abstract void defaultAttack(Unit unit);
