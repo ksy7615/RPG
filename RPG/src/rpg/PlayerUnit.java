4242faa921f323ad42c0bc5026452be08fff85dc
ksy7615 <sykim7615@daum.net>
2024-04-02 10:26:42 +0900

    Add Constructor Of Class PlayerUnit

--- RPG/src/rpg/PlayerUnit.java
+++ RPG/src/rpg/PlayerUnit.java
@@ -1,5 +1,9 @@
 package rpg;
 
-abstract class PlayerUnit {
+abstract class PlayerUnit extends Unit {
+
+	public PlayerUnit(String name, int hp, int power, int defence, int level, int exp) {
+		super(name, hp, power, defence, level, exp);
+	}
 
 }
