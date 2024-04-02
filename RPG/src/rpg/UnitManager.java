7f3bdca1ed64c679eb35886191495161445d9605
ksy7615 <sykim7615@daum.net>
2024-04-02 17:08:14 +0900

    Add ArrayList(PlayerUnit, MonsterUnit) Of Class UnitManager

--- RPG/src/rpg/UnitManager.java
+++ RPG/src/rpg/UnitManager.java
@@ -1,5 +1,9 @@
 package rpg;
 
+import java.util.ArrayList;
+
 public class UnitManager {
+	private ArrayList<PlayerUnit> playerList = new ArrayList<>();
+	private ArrayList<MonsterUnit> monsterList = new ArrayList<>();
 
 }
