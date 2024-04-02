9a68d433c05dcd231ff0010e22d801f1734064be
ksy7615 <sykim7615@daum.net>
2024-04-02 15:05:07 +0900

    Modify extends Class Of Class Orc

--- RPG/src/rpg/Orc.java
+++ RPG/src/rpg/Orc.java
@@ -1,9 +1,15 @@
 package rpg;
 
-public class Orc extends Unit {
+public class Orc extends MonsterUnit {
 
-	public Orc(String name, int hp, int power, int defence) {
-		super(name, hp, power, defence);
+	public Orc(String name, int hp, int power, int defence, int level) {
+		super(name, hp, power, defence, level);
+	}
+
+	@Override
+	public void attackPlayer(Unit unit) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
