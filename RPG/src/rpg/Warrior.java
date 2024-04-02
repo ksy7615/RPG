6dd2b8d3a4a3124c8aa465088ad2acef0b6044a6
ksy7615 <sykim7615@daum.net>
2024-04-02 11:00:36 +0900

    Delete variable mp Of Class Warrior

--- RPG/src/rpg/Warrior.java
+++ RPG/src/rpg/Warrior.java
@@ -3,15 +3,10 @@ package rpg;
 public class Warrior extends PlayerUnit {
 	private int hit;
 	private int slice;
-
 	private int potion;
-	private final int MAX_MP;
-	private int mp;
 
 	public Warrior(String name, int hp, int mp, int power, int defence, int level, int exp, int potion) {
-		super(name, hp, power, defence, level, exp);
-		MAX_MP = mp;
-		this.mp = mp;
+		super(name, hp, mp, power, defence, level, exp);
 		this.potion = potion;
 	}
 
@@ -22,6 +17,7 @@ public class Warrior extends PlayerUnit {
 		if (this.hit == 0)
 			System.err.println("MISS");
 
+		// 몬스터의 방어력 비례 피해량 수정 필요
 		monster.setHp(monster.getHp() - this.hit);
 
 		if (monster.getHp() <= 0) {
@@ -32,10 +28,11 @@ public class Warrior extends PlayerUnit {
 		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.", monster.getName()));
 	}
 
+	// 용사의 스킬 > 공격력 2배 & 미스X 유효타
 	@Override
 	public void skill(Unit monster) {
-		// TODO Auto-generated method stub
-
+		this.slice = 2 * (random.nextInt(this.power) + 1);
+		
 	}
 
 	@Override
