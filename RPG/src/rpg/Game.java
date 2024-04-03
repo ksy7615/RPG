01898e0f94c752d0fcb48fa9285b8ecc1643942f
ksy7615 <sykim7615@daum.net>
2024-04-03 15:35:25 +0900

    Add changeStage method & this method input run method Of Class Game

--- RPG/src/rpg/Game.java
+++ RPG/src/rpg/Game.java
@@ -8,13 +8,38 @@ public class Game {
 	public static Scanner scanner = new Scanner(System.in);
 	Map<String, Stage> stageList = new HashMap<String, Stage>();
 	public static String nextStage = "";
+	private String curStage = "";
 	
 	public Game() {
 		stageList.put("BATTLE", new StageBattle());
 		stageList.put("LOBBY", new StageLobby());
+		
+		nextStage = "LOBBY";
+	}
+	
+	private boolean changeStage() {
+		if(curStage.equals(nextStage)) {
+			return true;
+		}
+		
+		curStage = nextStage;
+		Stage stage = stageList.get(curStage);
+		
+		boolean isRun = true;
+		while(true) {
+			isRun = stage.update();
+			if(!isRun)
+				break;
+		}
+		
+		if(nextStage.equals("")) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 	
 	public void run() {
-		
+		changeStage();
 	}
 }
