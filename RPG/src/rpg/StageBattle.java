6c4dc295d63cedaaeb7f1d8e0ee29fc06d16acd0
ksy7615 <sykim7615@daum.net>
2024-04-03 11:45:17 +0900

    Add printState Of Class StageBattle

--- RPG/src/rpg/StageBattle.java
+++ RPG/src/rpg/StageBattle.java
@@ -13,6 +13,18 @@ public class StageBattle extends Stage {
 		monsterList = unitManager.monsterList;
 	}
 	
+	private void printState() {
+		// 나중에 상태창 수정 필요
+		System.out.println("=== PLAYER ===");
+		for(int i=0; i<playerList.size(); i++) {
+			System.out.println(playerList.get(i));
+		}
+		System.out.println("=== MONSTER ===");
+		for(int i=0; i<monsterList.size(); i++) {
+			System.out.println(monsterList.get(i));
+		}
+	}
+	
 	@Override
 	public boolean update() {
 		return false;
