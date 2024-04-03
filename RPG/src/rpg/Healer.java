3d5dc52227952ceffafedff4be9ec8411df306fd
ksy7615 <sykim7615@daum.net>
2024-04-03 14:32:32 +0900

    Add defaultAttackBoss Of Class Healer

--- RPG/src/rpg/Healer.java
+++ RPG/src/rpg/Healer.java
@@ -24,6 +24,40 @@ public class Healer extends PlayerUnit {
 		}
 		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", monster.getName(), this.hit));
 	}
+	
+	@Override
+	public void defaultAttackBoss(Unit monster) {
+		System.out.println(String.format("[%s]의 기본 공격\n", super.getName()));
+		this.hit = random.nextInt(this.power);
+		
+		this.hit = this.hit - monster.getDefence()/5;
+		
+		if (this.hit == 0) {
+			System.err.println("MISS");
+			return;
+		}
+		
+		if(monster instanceof BossMob) {
+			if(((BossMob) monster).getShield() > 0) {
+				int broke = ((BossMob) monster).getShield() - this.hit;
+				
+				if(broke > 0) {
+					((BossMob) monster).setShield(broke);
+				} else {
+					((BossMob) monster).setShield(0);
+					monster.setHp(monster.getHp() - broke);
+				}
+			} else {
+				monster.setHp(monster.getHp() - this.hit);
+			}
+		}
+		
+		if(monster.getHp() <= 0) {
+			monster.setHp(0);
+		}
+		
+		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", monster.getName(), this.hit));
+	}
 
 	// 다른 파티 플레이어 피 100씩 회복 해주기
 	@Override
