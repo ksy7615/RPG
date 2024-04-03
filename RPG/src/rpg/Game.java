a8b20481390fca8885e07237a58cd46300768c36
ksy7615 <sykim7615@daum.net>
2024-04-03 11:06:01 +0900

    Add variable nextStage(for move nextStage) Of Class Game

--- RPG/src/rpg/Game.java
+++ RPG/src/rpg/Game.java
@@ -1,9 +1,18 @@
 package rpg;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Game {
 	public static Scanner scanner = new Scanner(System.in);
+	Map<String, Stage> stageList = new HashMap<String, Stage>();
+	public static String nextStage = "";
+	
+	public Game() {
+		stageList.put("BATTLE", new StageBattle());
+		stageList.put("LOBBY", new StageLobby());
+	}
 	
 	public void run() {
 		
