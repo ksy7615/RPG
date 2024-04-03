b14cf9c94dd5eff3cd4aee4cfaa28b2cd9944e1a
ksy7615 <sykim7615@daum.net>
2024-04-03 14:20:33 +0900

    Modify Defensive Proportional Attack Of Class Tanker

--- RPG/src/rpg/Tanker.java
+++ RPG/src/rpg/Tanker.java
@@ -24,6 +24,39 @@ public class Tanker extends PlayerUnit {
 		System.out.println(String.format("[%s]의 기본 공격\n", super.getName()));
 		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", monster.getName(), this.hit));
 	}
+	
+	private void defaultAttackBoss(Unit monster) {
+		System.out.println(String.format("[%s]의 기본 공격\n", super.getName()));
+		this.hit = random.nextInt(this.power);
+		
+		this.hit = this.hit - monster.getDefence()/5;
+		
+		if (this.hit == 0) {
+			System.err.println("MISS");
+			return;
+		}
+		
+		if(monster instanceof BossMob) {
+			if(((BossMob) monster).getShield() > 0) {
+				int broke = ((BossMob) monster).getShield() - this.hit;
+				
+				if(broke > 0) {
+					((BossMob) monster).setShield(broke);
+				} else {
+					((BossMob) monster).setShield(0);
+					monster.setHp(monster.getHp() - broke);
+				}
+			} else {
+				monster.setHp(monster.getHp() - this.hit);
+			}
+		}
+		
+		if(monster.getHp() <= 0) {
+			monster.setHp(0);
+		}
+		
+		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", monster.getName(), this.hit));
+	}
 
 	// 탱커 스킬 > 자기가 다 맞기
 	@Override
