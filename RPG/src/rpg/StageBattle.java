f116c8126501f2000fa725c4e13b7247d7329088
ksy7615 <sykim7615@daum.net>
2024-04-03 11:17:24 +0900

    Add Constructor(Setting) Of Class StageBattle

--- RPG/src/rpg/StageBattle.java
+++ RPG/src/rpg/StageBattle.java
@@ -7,6 +7,12 @@ public class StageBattle extends Stage {
 	private ArrayList<PlayerUnit> playerList = new ArrayList<>();
 	private ArrayList<MonsterUnit> monsterList = new ArrayList<>();
 
+	public StageBattle() {
+		unitManager.setGeneralMob(4);
+		playerList = unitManager.playerList;
+		monsterList = unitManager.monsterList;
+	}
+	
 	@Override
 	public boolean update() {
 		return false;
