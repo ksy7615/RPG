1e64d5dc655d2e33b49b8be86e602349549a4939
ksy7615 <sykim7615@daum.net>
2024-04-03 10:38:52 +0900

    Add setCerberus method Of Class UnitManager

--- RPG/src/rpg/UnitManager.java
+++ RPG/src/rpg/UnitManager.java
@@ -8,8 +8,8 @@ public class UnitManager {
 	private ArrayList<PlayerUnit> playerList = new ArrayList<>();
 	private ArrayList<MonsterUnit> monsterList = new ArrayList<>();
 	private String path = "rpg";
-	private String[] generalMonsters = {"Slime", "Mimic", "Goblin", "Orc"};
-	private String[] BossMonsters = {"Cerberus", "Hades"};
+	private String[] generalMonsters = { "Slime", "Mimic", "Goblin", "Orc" };
+	private String[] bossMonsters = { "Cerberus", "Hades" };
 
 	public UnitManager() {
 		// 1.체력 2.마나 3.공격력 4.방어력 5.레벨 6.경험치 7.포션
@@ -17,25 +17,39 @@ public class UnitManager {
 		Tanker tanker = new Tanker("탱커", 800, 200, 30, 80, 1, 0, 3);
 		Wizard wizard = new Wizard("마법사", 500, 500, 50, 30, 1, 0, 3);
 		Healer healer = new Healer("힐러", 500, 500, 30, 30, 1, 0, 3);
-		
+
 		playerList.add(warrior);
 		playerList.add(tanker);
 		playerList.add(wizard);
 		playerList.add(healer);
 	}
-	
+
 	private void setGeneralMob(int size) {
-		for(int i=0; i<size; i++) {
+		for (int i = 0; i < size; i++) {
 			int num = random.nextInt(generalMonsters.length);
 			try {
 				Class<?> clazz = Class.forName(path + generalMonsters[num]);
 				Object obj = clazz.getDeclaredConstructor().newInstance();
-				
+
 				MonsterUnit temp = (MonsterUnit) obj;
 				monsterList.add(temp);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+		}
+	}
+
+	private void setCerberus() {
+		try {
+			Class<?> clazz = Class.forName(path + bossMonsters[0]);
+			Class<?>[] params = new Class<?>[] { int.class };
+
+			Object obj = clazz.getDeclaredConstructor(params).newInstance(5000);
+
+			MonsterUnit temp = (MonsterUnit) obj;
+			monsterList.add(temp);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
