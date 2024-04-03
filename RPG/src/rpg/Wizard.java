df3b8ce21018947b2ed15f030ec902bc7c48b263
ksy7615 <sykim7615@daum.net>
2024-04-03 14:31:13 +0900

    Add defaultAttackBoss Of Class Wizard

--- RPG/src/rpg/Wizard.java
+++ RPG/src/rpg/Wizard.java
@@ -11,7 +11,7 @@ public class Wizard extends PlayerUnit {
 	@Override
 	public void defaultAttack(Unit monster) {
 		this.hit = random.nextInt(this.power);
-
+		
 		if (this.hit == 0)
 			System.err.println("MISS");
 
@@ -23,6 +23,40 @@ public class Wizard extends PlayerUnit {
 		}
 
 		System.out.println(String.format("[%s]의 기본 공격\n", super.getName()));
+		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", monster.getName(), this.hit));
+	}
+	
+	@Override
+	public void defaultAttackBoss(Unit monster) {
+		System.out.println(String.format("[%s]의 기본 공격\n", super.getName()));
+		this.hit = random.nextInt(this.power);
+		
+		this.hit = this.hit - monster.getDefence()/5;
+		
+		if (this.hit == 0) {
+			System.err.println("MISS");
+			return;
+		}
+		
+		if(monster instanceof BossMob) {
+			if(((BossMob) monster).getShield() > 0) {
+				int broke = ((BossMob) monster).getShield() - this.hit;
+				
+				if(broke > 0) {
+					((BossMob) monster).setShield(broke);
+				} else {
+					((BossMob) monster).setShield(0);
+					monster.setHp(monster.getHp() - broke);
+				}
+			} else {
+				monster.setHp(monster.getHp() - this.hit);
+			}
+		}
+		
+		if(monster.getHp() <= 0) {
+			monster.setHp(0);
+		}
+		
 		System.out.println(String.format("[%s]에게 [%d]만큼의 데미지를 입혔다.\n", monster.getName(), this.hit));
 	}
 
